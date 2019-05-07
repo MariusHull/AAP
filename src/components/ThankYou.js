960c738c0b4a65d90c8c41e6262c87bf39dcba34
Nicolas GABRION <nico.gabrion@gmail.com>
2019-05-07 17:04:40 +0200

    IMPROVE: data access and page thank you

---
+++ src/components/ThankYou.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import {
+  Container,
+} from "semantic-ui-react";
+
+import NavBar from "./NavBar";
+
+export default class ThankYou extends Component {
+
+  render() {
+
+    return (
+      <Container style={{width:'100%'}}>
+      <NavBar />
+      <h1 style={{position: 'absolute', top: '40vh', left:'25vw', margin:'auto', textAlign:'center', width:'50vw'}}>Merci d'avoir complété ce questionnaire. Vous pouvez désormais fermer l'onglet.</h1>
+      </Container>
+    );
+  }
+}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "../components/Home";
 import Survey from "../components/Survey";
 import Admin from "../components/Admin";
+import ThankYou from "../components/ThankYou";
 
 class Routes extends Component {
   render() {
@@ -12,6 +13,7 @@ class Routes extends Component {
         <Route path="/" exact component={Home} />
         <Route path="/survey" exact component={Survey} />
         <Route path="/admin" exact component={Admin} />
+        <Route path="/thankyou" exact component={ThankYou} />
       </Router>
     );
   }
