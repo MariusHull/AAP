31280143f59173133d5cbf6cf8acb73d3f85ea46
Quentin Blampey <quentinblampey@Quentins-MacBook-Pro.local>
2019-06-25 22:14:05 +0200

    c nav

---
+++ src/components/CompanyBar.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class CompanyBar extends Component {
+  render() {
+    return (
+      <div style={{ width: "300px" }}>
+        <p> Hi </p>
+      </div>
+    );
+  }
+}
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -6,55 +6,60 @@ import Sites from "../components/Company/Sites";
 import Populations from "../components/Company/Populations";
 import Background from "../assets/Bureau_38.jpg";
 import NavBar from "../components/NavBar";
+import CompanyBar from "../components/CompanyBar";
 
 class Routes extends Component {
   render() {
     return (
       <Container style={{ width: "100%" }}>
         <NavBar />
-        <Container
-          style={{
-            width: "100%",
-            height: "auto",
-            backgroundImage: `url(${Background})`,
-            backgroundSize: "cover"
-          }}
-        >
-          <div
-            className="container"
+        <div style={{ display: "flex" }}>
+          <CompanyBar />
+          <Container
             style={{
-              display: "flex",
-              height: "calc(100vh - 95px)",
-              flexDirection: "column",
-              justifyContent: "center"
+              width: "100%",
+              height: "auto",
+              backgroundImage: `url(${Background})`,
+              backgroundSize: "cover"
             }}
           >
-            <Container
+            <div
               className="container"
               style={{
-                width: "80vw",
-                background: "rgba(255,255,255,0.9)",
-                borderRadius: "3px"
+                display: "flex",
+                height: "calc(100vh - 95px)",
+                width: "calc(100vw - 300px)",
+                flexDirection: "column",
+                justifyContent: "center"
               }}
             >
-              <Router>
-                <div style={{ marginBottom: "50px" }}>
-                  <Route
-                    path="/survey/:indexSite,:indexPopulation"
-                    exact
-                    component={Survey}
-                  />
-                  <Route path="/sites" exact component={Sites} />
-                  <Route
-                    path="/population/:index"
-                    exact
-                    component={Populations}
-                  />
-                </div>
-              </Router>
-            </Container>
-          </div>
-        </Container>
+              <Container
+                className="container"
+                style={{
+                  width: "80vw",
+                  background: "rgba(255,255,255,0.9)",
+                  borderRadius: "3px"
+                }}
+              >
+                <Router>
+                  <div style={{ marginBottom: "50px" }}>
+                    <Route
+                      path="/survey/:indexSite,:indexPopulation"
+                      exact
+                      component={Survey}
+                    />
+                    <Route path="/sites" exact component={Sites} />
+                    <Route
+                      path="/population/:index"
+                      exact
+                      component={Populations}
+                    />
+                  </div>
+                </Router>
+              </Container>
+            </div>
+          </Container>
+        </div>
       </Container>
     );
   }
