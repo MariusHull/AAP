69f62b4f4aa8f423059f299ac981b56b6db58c42
MariusHull <marius.hullin@gmail.com>
2019-06-26 12:28:40 +0100

    Update Export.js

--- src/components/Admin/Export.js
+++ src/components/Admin/Export.js
@@ -15,8 +15,30 @@ export default class Export extends React.Component {
       }
     ];
 
+    const dataSetPage1 = [
+      {
+        columns: [
+          { title: "Nom de l'entreprise", width: { wpx: 80 } },
+          { title: "Identifiant", width: { wpx: 80 } }
+        ],
+        data: [
+          {
+            value: this.props.company.name,
+            style: { font: { sz: "12", bold: true } }
+          },
+          this.props.company._id
+        ]
+      }
+    ];
+    const now = new Date();
+    const title = `${
+      this.props.company.name
+    }_${now.getDate()}/${now.getMonth() + 1}/${now.getFullYear()}@${
+      now.getHours
+    }:${now.getMinutes()}`;
     return (
       <ExcelFile
+        filename={title}
         element={
           <Button icon positive labelPosition="right">
             Télécharger les réponses
@@ -24,7 +46,8 @@ export default class Export extends React.Component {
           </Button>
         }
       >
-        <ExcelSheet data={dataSet1} name="Réponses au questionnaire">
+        <ExcelSheet data={dataSetPage1} name="Feuille 1" />
+        <ExcelSheet data={dataSet1} name="Feuille 2">
           <ExcelColumn label="Name" value="name" />
           <ExcelColumn label="Id" value="id" />
         </ExcelSheet>
