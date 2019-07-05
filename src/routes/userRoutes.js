149d02708e86fb23ad0a6825b28361cb04441ece
MariusHull <marius.hullin@gmail.com>
2019-07-05 13:00:46 +0100

    fix : design settings

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Container } from "semantic-ui-react";
 import Survey from "../components/Company/Survey";
 import Sites from "../components/Company/Sites";
@@ -13,41 +13,43 @@ class UserRoutes extends Component {
   render() {
     return (
       <Router>
-        <Container style={{ width: "100%" }}>
-          <NavBar />
-          <Container
-            style={{
-              width: "100%",
-              height: "calc(100vh - 95px)",
-              position: "absolute",
-              top: "53px",
-              backgroundImage: `url(${Background})`,
-              backgroundSize: "cover"
-            }}
-          >
+        <Switch>
+          <Route path="/settings" exact component={Settings} />
+          <Container style={{ width: "100%" }}>
+            <NavBar />
             <Container
-              className="container"
               style={{
-                width: "80vw",
-                marginTop: "30px",
-                background: "rgba(255,255,255,0.9)",
-                borderRadius: "3px"
+                width: "100%",
+                height: "calc(100vh - 95px)",
+                position: "absolute",
+                top: "53px",
+                backgroundImage: `url(${Background})`,
+                backgroundSize: "cover"
               }}
             >
-              <div style={{ marginBottom: "50px" }}>
-                <Route
-                  path="/user/survey/:indexSite,:indexPopulation"
-                  exact
-                  component={Survey}
-                />
-                <Route path="/user/thankyou" exact component={ThankYou} />
-                <Route path="/user/sites" exact component={Sites} />
-                <Route path="/" exact component={ThankYou} />
-                <Route path="/settings" exact component={Settings} />
-              </div>
+              <Container
+                className="container"
+                style={{
+                  width: "80vw",
+                  marginTop: "30px",
+                  background: "rgba(255,255,255,0.9)",
+                  borderRadius: "3px"
+                }}
+              >
+                <div style={{ marginBottom: "50px" }}>
+                  <Route
+                    path="/user/survey/:indexSite,:indexPopulation"
+                    exact
+                    component={Survey}
+                  />
+                  <Route path="/user/thankyou" exact component={ThankYou} />
+                  <Route path="/user/sites" exact component={Sites} />
+                  <Route path="/" exact component={ThankYou} />
+                </div>
+              </Container>
             </Container>
           </Container>
-        </Container>
+        </Switch>
       </Router>
     );
   }
